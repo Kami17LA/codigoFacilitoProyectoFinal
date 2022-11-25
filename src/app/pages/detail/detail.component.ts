e94ef629aa106fc4d2a9f150446fbab7f8c310cd
Katherine Milagros <katherinebemol17@gmail.com>
2022-11-25 11:38:45 -0500

    Se implemento la funcionalidad para buscar cartas

---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { Card } from 'src/app/interfaces/card.interface';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class DetailComponent implements OnInit {
+
+  id!: string;
+  /* es como un subscribe */
+  card$!: Observable<Card>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cardService: CardService
+    ) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    console.log(this.id);
+    this.card$ = this.cardService.getCard(this.id).pipe(tap(console.log));
+  }
+
+}
